89f41c874bba8baf0f51f60e70402e0ea0e82443
Amin Khan <154885880+Aminkhan-mid@users.noreply.github.com>
2025-10-27 12:11:53 +0530

    now the coffee cards work too.

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ userGreet.textContent = random
 
 coffeeCards.forEach(card =>{
     const uuid = crypto.randomUUID()
+    card.uuid = uuid
        coffeeCardsContainer.innerHTML += `
        <div class="coffee-card">
             <img 
@@ -207,7 +208,168 @@ document.addEventListener("click", (e)=>{
         })
     })
     } 
+
+
+    
+document.addEventListener("click", (e)=>{
+    if(e.target.dataset.type === "card"){
+        const clickedID = e.target.dataset.id
+        const selectedCard = coffeeCards.find(card => card.uuid === clickedID)
+        if(!selectedCard) return console.log("⚠️ No Coffee found for that ID")
+        const {price, name, img, keyIngredient, initText, description, ratings} = selectedCard
+
+        app.innerHTML = `
+             <div class="backLike-div">
+        <div class="backLike-iconDiv">
+            <a href="../index.html" style="color: antiquewhite;">
+                <i class="fa-solid fa-angle-left"></i>
+            </a>
+        </div>
+        <div class="backLike-iconDiv">
+            <i class="fa-solid fa-heart" menuHeart></i>
+        </div>
+    </div>
+    <div class="coffeeDetails-container">
+        <div class="bg-img" style="background-image:url('${img}');"></div>
+        <span class="details-flexOne">
+            <div class="food-inDetails">
+                <span class="ratings-flex">
+                    <p class="menu-ratings">
+                        <i class="fa-solid fa-star"></i> ${ratings} 
+                    </p>
+                    <p class="reviews">(250 reviews)</p>
+                </span>
+
+                        <p class="menu-name">${name}</p>
+                        <p class="key-ingredient">${keyIngredient}</p>
+            </div>
+            <div>
+                <p class="price-p">Price</p>
+                <p class="menu-price">₹ ${price}</p>
+            </div>
+        </div>
+
+
+        <div class="details-two">
+            <input id="ch" type="checkbox">
+            <p class="description-p">Description</p>
+            <p class="initial-text">
+                ${initText}...
+            </p>
+            <div class="content">
+                <p class="full-text">
+                    ${description}
+                </p>
+                <label for="ch">Show less</label>
+            </div>
+            <label for="ch">Read more</label>
+        </div>
+
+        <span class="dropdown-container">
+
+            <div class="dropdown">
+                <div class="select">
+                    <span class="selected">Select Size</span>
+                    <div class="caret"></div>
+                </div>
+                <ul class="menu">
+                    <li>Small</li>
+                    <li class="active">Medium</li>
+                    <li>Large</li>
+                </ul>
+            </div>
+
+
+            <div class="dropdown">
+                <div class="select">
+                    <span class="selected">Milk Type</span>
+                    <div class="caret"></div>
+                </div>
+                <ul class="menu">
+                    <li>Oat</li>
+                    <li class="active">Cow</li>
+                    <li>Almond</li>
+                    <li>Goat</li>
+                </ul>
+            </div>
+
+            <div class="dropdown">
+                <div class="select">
+                    <span class="selected">Sweetness</span>
+                    <div class="caret"></div>
+                </div>
+                <ul class="menu">
+                    <li>Less Sugar</li>
+                    <li>Regular Sugar</li>
+                    <li>Extra Sugar</li>
+                    <li class="active">No Sugar</li>
+                </ul>
+            </div>
+
+            <div class="dropdown">
+                <div class="select">
+                    <span class="selected">Temperature</span>
+                    <div class="caret"></div>
+                </div>
+                <ul class="menu">
+                    <li>Cold</li>
+                    <li>Normal</li>
+                    <li class="active">Hot</li>
+                </ul>
+            </div>
+        </span>
+
+        <div class="qntDiv-flex">
+            <span>
+                <div class="spanDivs">
+                    <i class="fa-solid fa-minus"></i>
+                </div>
+                
+                <p>1</p>
+                <div class="spanDivs">
+                    <i class="fa-solid fa-plus"></i>
+                </div>
+            </span>
+            <button class="buyNow-btn">Buy Now</button>
+        </div>
+
+
+
+    </div>`
+    const dropdowns = document.querySelectorAll('.dropdown')
+    dropdowns.forEach(dropdown => {
+        const select = dropdown.querySelector('.select')
+        const caret = dropdown.querySelector('.caret')
+        const menu = dropdown.querySelector('.menu')
+        const options = dropdown.querySelectorAll('.menu li')
+        const selected = dropdown.querySelector('.selected')
+        
+        select.addEventListener("click", ()=>{
+            select.classList.toggle('select-clicked')
+            caret.classList.toggle('caret-rotate')
+            menu.classList.toggle('menu-open')
+        })
+        options.forEach(option => {
+            option.addEventListener("click", () => {
+                selected.innerText = option.innerText
+                select.classList.remove('select-clicked')
+                caret.classList.remove('caret-rotate')
+                menu.classList.remove('menu-open')
+                
+                options.forEach(opt => opt.classList.remove('active'))
+                option.classList.add('active')
+            })
+        })
+    })
+    } 
 })
+
+
+
+
+})
+
+
 
 
 
